fd16751738f8aa35e2696329dd2481561495695d
Cait <cait.h.crawford@gmail.com>
2017-11-22 20:25:48 -0500

    Created new software that allows for smoother arm movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MavArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MavArm.java
@@ -2,13 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.Servo.Direction;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import static android.R.attr.left;
 import static android.R.attr.x;
+import static android.os.Build.VERSION_CODES.M;
 import static org.firstinspires.ftc.teamcode.ButtonLogic.changeTime;
+import static org.firstinspires.ftc.teamcode.MaverbitsTeleOp.ARM_POWER;
 
 /**
  * Created by Henry on 11/22/2017.
@@ -17,6 +22,11 @@ import static org.firstinspires.ftc.teamcode.ButtonLogic.changeTime;
 @TeleOp
 public class MavArm extends OpMode
 {
+
+    boolean upPressed;
+    boolean downPressed;
+
+    DcMotor arm;
 
     boolean rBumpPressed;
     boolean lBumpPressed;
@@ -35,31 +45,56 @@ public class MavArm extends OpMode
     double totTime = 1;
     double velocity;
 
+    double armStartPos = 0;
+    double armEndPos = 1100;
+    double armTime = 2;
+    double armVelocity;
+    double armDistance = 0;
+
     @Override
     public void init()
     {
 
         leftClaw = hardwareMap.servo.get("leftClaw");
         rightClaw = hardwareMap.servo.get("rightClaw");
+        arm = hardwareMap.dcMotor.get("arm");
 
         rightClaw.setDirection(Direction.REVERSE);
+
+        arm.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+
+        arm.setMode(RunMode.RESET_ENCODERS);
+
+        arm.setMode(RunMode.RUN_USING_ENCODER);
 
         rBumpPressed = false;
         lBumpPressed = false;
 
+        upPressed = false;
+        downPressed = false;
+
         startTime = this.getRuntime();
 
         velocity = (endPos - startPos) / totTime;
+        armVelocity = (armEndPos - armStartPos) / armTime;
 
         leftClaw.setPosition(distance);
         rightClaw.setPosition(distance);
 
+        arm.setTargetPosition((int) armDistance);
+
     }
 
+    boolean bFirstIteration = true;
     @Override
     public void loop()
     {
 
+        if(bFirstIteration) {
+            bFirstIteration = false;
+            arm.setMode(RunMode.RUN_TO_POSITION);
+            arm.setPower(ARM_POWER);
+        }
         currTime = this.getRuntime();
 
         elapsedTime = currTime - startTime;
@@ -157,7 +192,98 @@ public class MavArm extends OpMode
 
             }
         }
+        if(gamepad1.dpad_up)
+        {
+            if(upPressed == false) {
+                upPressed = true;
 
+                startTime = currTime;
+
+            }
+            else
+            {
+                if(elapsedTime >= changeTime)
+                {
+                    if(armDistance < 1100)
+                    {
+                        armDistance = armDistance + (elapsedTime * armVelocity);
+
+                        startTime = currTime;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+
+        }
+        else if(!gamepad1.dpad_up)
+        {
+            if(upPressed == true)
+            {
+                upPressed = false;
+
+            }
+            else
+            {
+
+            }
+        }
+
+        if(gamepad1.dpad_down)
+        {
+            if(downPressed == false) {
+                downPressed = true;
+
+                startTime = currTime;
+
+
+                RobotLog.vv("Left Bumper", "Bumper Just Pressed");
+            }
+            else
+            {
+                if(elapsedTime >= changeTime)
+                {
+                    if(armDistance > 10)
+                    {
+                        armDistance = armDistance - (elapsedTime * armVelocity);
+
+                        startTime = currTime;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+
+        }
+        else if(!gamepad1.dpad_down)
+        {
+            if(downPressed == true)
+            {
+                downPressed = false;
+
+            }
+            else
+            {
+
+            }
+        }
+
+
+        arm.setTargetPosition((int) armDistance);
         leftClaw.setPosition(distance);
         rightClaw.setPosition(distance);
 
